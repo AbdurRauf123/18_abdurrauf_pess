51164dd148686f7d5130646d754aab45679adeab
AbdurRauf123 <68846837+AbdurRauf123@users.noreply.github.com>
2021-02-21 13:38:44 +0800

    6th Version - Updated navbar

--- loginheader.php
+++ loginheader.php
@@ -7,26 +7,9 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent1">
         <ul class="navbar-nav mr-auto">
 			
-          <li class="nav-item"> <a class="nav-link" href="login.php">Home <span class="sr-only">(current)</span></a> </li>
+          <li class="nav-item active"> <a class="nav-link" href="login.php">Login <span class="sr-only">(current)</span></a> </li>
 			
-          <li class="nav-item"> <a class="nav-link" href="login.php">Dispatch</a> </li>
-			
-		  <li class="nav-item"> <a class="nav-link" href="login.php">Update</a> </li>
-			
-		  <li class="nav-item"> <a class="nav-link" href="login.php">Search</a> </li>
-			
-          <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="login.php" id="navbarDropdown1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Reports </a>
-			  
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown1"> <a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a>
-				
-              <div class="dropdown-divider"></div>
-				
-              <a class="dropdown-item" href="#">Something else here</a> </div>
-          </li>
-        </ul>
-		<ul class="navbar-nav my-2 my-sm-0">
-		  <li class="nav-item active "> <a class="nav-link " href="login.php">Login <span class="sr-only">(current)</span></a> </li>
-		</ul>		  
+        </ul>		  
       </div>
     </nav>
   </header>
